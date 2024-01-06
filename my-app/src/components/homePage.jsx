0244b7ffaaa64e1a6279e4fc6de1a9d8b7b8e988
PheakdeyKim <Pheakdeykim03@gmail.com>
2024-01-06 10:40:28 +0700

    Updated

--- my-app/src/components/homePage.jsx
+++ my-app/src/components/homePage.jsx
@@ -8,65 +8,65 @@ import DoctorCard from "./ui/doctorCard";
 import { useLocation } from "react-router-dom";
 
 const HomePage = () => {
-	const location = useLocation();
-	return (
-		<div>
-			<TopBar />
-			<NavBar />
-			<section>
-				<div className="home-page-img-doctor1">
-					<div>
-						<img src="/img/homepage_doc.jpeg" alt="HomePage_Doctor1" />
-					</div>
-				</div>
-				<div className="appoinment-button">
-					<a href="bookingappointment">
-						<button
-							id="Appointment"
-							type="button"
-							className="btn btn-success btn-lg"
-						>
-							Appointment
-						</button>
-					</a>
-					<a href="service">
-						<button id="Services" type="button" className="btn btn-lg">
-							Services
-						</button>
-					</a>
-				</div>
-				<div className="trustd-medical-clinic-in-cambodia">
-					<p>Trusted Medical Clinic in Cambodia</p>
-					<div id="verman-hospital">
-						<h1>DOCPRO</h1>
-					</div>
-				</div>
-			</section>
-			<section className="pageket-section">
-				<div id="pageket-title-section">
-					<h1>PAGEKET</h1>
-					<p>Professional. So quality. So friendly.</p>
-				</div>
-				<div className="wrapper">
-					<div className="container">
-						<div className="product">
-							<div className="product-container">
-								<DoctorCard />
-								<DoctorCard />
-								<DoctorCard />
-								<DoctorCard />
-								<DoctorCard />
-								<DoctorCard />
-								<DoctorCard />
-								<DoctorCard />
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-			<Footer />
-		</div>
-	);
+  const location = useLocation();
+  return (
+    <div>
+      <TopBar />
+      <NavBar />
+      <section>
+        <div className="home-page-img-doctor1">
+          <div>
+            <img src="/img/homepage_doc.jpeg" alt="HomePage_Doctor1" />
+          </div>
+        </div>
+        <div className="appoinment-button">
+          <a href="bookingappointment">
+            <button
+              id="Appointment"
+              type="button"
+              className="btn btn-success btn-lg"
+            >
+              Appointment
+            </button>
+          </a>
+          <a href="service">
+            <button id="Services" type="button" className="btn btn-lg">
+              Services
+            </button>
+          </a>
+        </div>
+        <div className="trustd-medical-clinic-in-cambodia">
+          <p>Trusted Medical Clinic in Cambodia</p>
+          <div id="verman-hospital">
+            <h1>DOCPRO</h1>
+          </div>
+        </div>
+      </section>
+      <section className="pageket-section">
+        <div id="pageket-title-section">
+          <h1>PAGEKET</h1>
+          <p>Professional. So quality. So friendly.</p>
+        </div>
+        <div className="wrapper">
+          <div className="container">
+            <div className="product">
+              <div className="product-container">
+                <DoctorCard />
+                <DoctorCard />
+                <DoctorCard />
+                <DoctorCard />
+                <DoctorCard />
+                <DoctorCard />
+                <DoctorCard />
+                <DoctorCard />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
 };
 
 export default HomePage;
